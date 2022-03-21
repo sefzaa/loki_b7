0366a36162b2782e560fe2510ce05ae90637d17d
Salmaslm13 <1304salma@gmail.com>
2022-03-21 13:48:43 +0700

    uji coba 2

--- Kelompok 7/no9sampai12.js
+++ Kelompok 7/no9sampai12.js
@@ -5,18 +5,18 @@ no9sampai12.listen (3000,function(){
     console.log("Server sudah hidup");
 });
 
-no9sampai12.get("/CPMK/add", function(req,res){
+no9sampai12.get("/addCPMK", function(req,res){
     res.send("CPMK berhasil ditambahkan")
 })
 
-no9sampai12.get("/CPMK/edit", function(req,res){
+no9sampai12.get("/editCPMK", function(req,res){
     res.send("CPMK berhasil diperbaharui")
 })
 
-no9sampai12.get("/CPMK/delete", function(req,res){
+no9sampai12.get("/deleteCPMK", function(req,res){
     res.send("CPMK Berhasil dihapus")
 })
 
-no9sampai12.get("/Reference/add", function(req,res){
+no9sampai12.get("/addReference", function(req,res){
     res.send("Referensi berhasil ditambahkan")
 })
