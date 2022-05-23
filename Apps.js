175f4f5dd05faf8b3c262bbae9ec7e4b1b8da135
Daribsan <dariyarig@gmail.com>
2022-05-23 11:59:52 +0700

    Log In

--- Apps.js
+++ Apps.js
@@ -1,15 +1,3 @@
-const express = require ('express');
-const web = express();
-
-web.get("/", function(req,res){
-    res.send("Kelompok 7 Pweb")
-    
-})
-
-web.listen (3000,function(){
-    console.log("Server Ready");
-});
-
 
 //
 web.get("/cetak_laporan", function(req,res){
